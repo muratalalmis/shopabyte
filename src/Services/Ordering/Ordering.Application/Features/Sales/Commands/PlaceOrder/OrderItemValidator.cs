0b773d703ccbdce3d670d54fa8b579e6e4a6c70a
Murat Alalmış <muratalalmis@outlook.com>
2022-01-09 04:26:03 +0300

    validations

---
+++ src/Services/Ordering/Ordering.Application/Features/Sales/Commands/PlaceOrder/OrderItemValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Sales.Commands.PlaceOrder
+{
+    public class OrderItemValidator : AbstractValidator<OrderItem>
+    {
+        public OrderItemValidator()
+        {
+            RuleFor(x => x.ProductId)
+                .NotNull()
+                .Empty();
+
+            RuleFor(x => x.Quantity)
+              .NotNull()
+              .Empty();
+
+            RuleFor(x => x.Price)
+              .NotNull()
+              .Empty();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Sales/Commands/PlaceOrder/PlaceOrderCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Sales.Commands.PlaceOrder
+{
+    public class PlaceOrderCommandValidator : AbstractValidator<PlaceOrderCommand>
+    {
+        public PlaceOrderCommandValidator()
+        {
+            RuleFor(x => x.Currency)
+                .NotNull()
+                .Empty()
+                .MaximumLength(3);
+
+            RuleFor(x => x.CustomerId)
+                .NotNull()
+                .Empty();
+
+            RuleFor(x => x.Total)
+                .NotNull()
+                .Empty();
+
+            RuleFor(x => x.Items)
+                .NotNull()
+                .Empty();
+        }
+    }
+}
