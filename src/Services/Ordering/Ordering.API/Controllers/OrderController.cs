67d99da46f840d80aa9523bcced0b243dbfcbdc5
Murat Alalmış <muratalalmis@outlook.com>
2022-01-09 04:42:34 +0300

    presentation

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Sales.Commands.PlaceOrder;
+using Ordering.Application.Features.Sales.Queries.GetSalesByDate;
+using System.Net;
+
+namespace Ordering.API.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetSalesBetweenDate")]
+        [ProducesResponseType(typeof(IEnumerable<SalesDto>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<SalesDto>>> GetSalesBetweenDate([FromQuery] DateTime beginDate, [FromQuery] DateTime endDate)
+        {
+            var query = new GetSalesByDateQuery(beginDate, endDate);
+            var sales = await _mediator.Send(query);
+
+            return Ok(sales);
+        }
+
+        [HttpPost(Name = "PlaceOrder")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<string>> PlaceOrder([FromBody] PlaceOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Features/Sales/Queries/GetSalesByDate/GetSalesByDateQuery.cs
+++ src/Services/Ordering/Ordering.Application/Features/Sales/Queries/GetSalesByDate/GetSalesByDateQuery.cs
@@ -4,6 +4,12 @@ namespace Ordering.Application.Features.Sales.Queries.GetSalesByDate
 {
     public class GetSalesByDateQuery : IRequest<List<SalesDto>>
     {
+        public GetSalesByDateQuery(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
